35ca0c1e644b874fe32543046c45cc2288c397de
Lee Goddard <lee@leegoddard.net>
2014-09-06 20:52:03 +0100

    Initial

---
+++ app/config.js
@@ -0,0 +1,34 @@
+require.config({
+    // to depend on a bower installed component:
+    // define(['componentName/file'])
+    baseUrl: 'bower_components/',
+    paths: {
+        // The ./app directory
+        "app": "../app/",
+        // The ./lib directory
+        "lib": "../lib/"
+    }
+});
+
+// http://www.w3.org/TR/animation-timing/
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          window.oRequestAnimationFrame      ||
+          window.msRequestAnimationFrame     ||
+          function ( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+if (!window.requireTestMode) {
+  require(['app/main'], function(){ });
+}
+
+
+
+
+
---
+++ app/main.js
@@ -0,0 +1,9 @@
+require(['lib/PcmVisual'], function (PcmVisual) {
+    new PcmVisual({
+        uri:            'samples/ouds/oud1.wav',
+        element:        'app-container',
+        rgbForeground:  '195,151,76',
+        height:         500,
+        width:          1100
+    });
+});
